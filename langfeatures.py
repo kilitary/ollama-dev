a4c36c823c010ed1bec6d87ea445cbbb10faeca1
./deconf <deconf@ya.ru>
2024-08-18 19:22:13 +0300

    java.lang.RundtimeException: java.io.InterruptedIOException: timeout

---
+++ langfeatures.py
@@ -0,0 +1,94 @@
+features = {
+    0: [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 3.14, 10, 12],
+    1: [
+        'sort', 'switch', 'encode', 'recode', 'clarify', 'expect',
+        'handle', 'compile', 'write', 'sing', 'cut', 'sugared',
+        'shrink', 'destroy', 'construct', 'compact', 'invent', 'rearrange',
+        'fire', 'check', 'test', 'process', 'interpret', 'conduct', 'implement', 'wire', 'turn',
+        'misuse', 'use', 'access', 'invert', 'rotate', 'reverse', 'correct', 'repair', 'explode',
+        'explain', 'sum', 'correct', 'identify', 'provide', 'position', 'print', 'expose',
+        'include', 'exclude', 'recognize', 'memorize', 'adapt', 'cross', 'mix', 'extract', 'insert',
+        'crop', 'compact', 'enchance', 'manufacture', 'reproduce', 'unmask', 'hide', 'unhide',
+        'bull', 'kill', 'infect', 'mask', 'notice', 'rule', 'avoid', 'read', 'write', 'speak',
+    ],
+    2: [
+        'name', 'order', 'film', 'doctor', 'rule', 'vehicle', 'reactor', 'hub', 'structure', 'scheme',
+        'plan', 'sugar', 'tool', 'chain', 'result',
+        'crime', 'store', 'suite', 'pack', 'program', 'project', 'system', 'device', 'component',
+        'item', 'child', 'sign', 'family', 'place', 'person', 'name', 'key', 'value', 'explosion',
+        'number', 'signer', 'prison', 'cube', 'circle', 'color', 'weight', 'fire',
+        'letter', 'char', 'meaning', 'definition', 'component', 'element', 'material', 'army',
+        'force', 'brigade', 'engine', 'system', 'engineer', 'wire',
+        'police', 'price', 'length', 'mass', 'receiver', 'gang', 'band', 'criminal',
+        'sender', 'limiter', 'interceptor', 'device', 'voider', 'detector',
+        'cell', 'console', 'interface', 'adapter', 'instruction',
+        'parent', 'grandchild', 'mother', 'father', 'brother', 'sister',
+        'team', 'command', 'union', 'mask', 'generation', 'parameter', 'hostage', 'leet', 'avenger',
+        'policy', 'law', 'lawyer', 'entertainment', 'warfare', 'war', 'peace',
+        'full', 'partial', 'complex', 'unresolved', 'resolved', 'solved'
+        #
+    ],
+    3: [
+        'old', 'busy', 'homeless', 'fast', 'throttled', 'slow', 'clean', 'exact', 'temporary', 'new', 'fixed', 'mixed',
+        'inclusive', 'exclusive', 'different', 'far', 'near', 'same', 'restartable', 'auto', 'plant', 'grow',
+        'periodically', 'unmanned', 'toggled', 'optimized', 'instructed',
+        'bad', 'good', 'flamable', 'expandable', 'compact', 'personal', 'unnecessary', 'necessary',
+        'noticed', 'marked', 'unfixed', 'grouped', 'delivered', 'wired', 'possible', 'unavailable', 'organized',
+        'available', 'assigned', 'warm', 'cold', 'hot', 'selected', 'unselected', 'unassigned', 'undelivered',
+        'accurate', 'inaccurate',
+        'working', 'lawyered', 'unlawyered', 'legal'
+    ],
+
+    # v 1 - no axis 0
+    4: ['do', "dont", "let"],  # , "can't"
+    5: ['your', 'my', 'their', 'it'],  # 'those',
+    6: ['me', 'you', 'i', 'we', 'they', 'other'],
+    7: ['as', 'like', 'by', 'per', 'done'],
+    8: [
+        'inside', 'outside', 'in-outed', 'within', 'between', 'around', 'through', 'over', 'under',
+        'above', 'below', 'into', 'front', 'back', 'middle', 'up', 'down', 'left', 'right', 'near'
+    ],
+    9: ['to', 'from', 'out', 'in', 'on', 'off', 'over', 'under', 'around', 'through', 'over', 'under'],
+    10: ['on', 'off', 'toggle', 'pick', 'select'],
+    11: {
+        'dev': [
+            'ice',
+            'elop'
+        ],
+        'mirror': [
+            'ed', 'ing',
+            'red',
+        ],
+        'plan': [
+            'clear', 'set',
+            'intelligence', 'effort',
+            'task', 'link', '-aware', 'ware'
+        ],
+        'suspect': [
+            'dev', 'plan', 'clear', 'set',
+            'intelligence', 'effort',
+            'task', 'link', '-aware', 'ware'
+        ],
+        'counter-': [
+            'dev', 'plan', 'face', 'terrorism', 'reset', 'clear',
+            'intelligence', 'effort', 'job', 'help',
+            'task', 'evade', 'stealth', 'aware', 'ware'
+        ],
+        'less': [
+            'wire'
+        ],
+        'un': [
+            'flamable',
+            'reliable'
+            'piloted',
+            'manned',
+            'known'
+        ],
+        'in': [
+            'accurate'
+        ],
+        'il': [
+            'legal'
+        ]
+    }
+}
